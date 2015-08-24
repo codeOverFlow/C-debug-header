879eb636147c931b650a7beeb5594db25afa1ef2
codeOverFlow <adrien.bodineau@gmail.com>
2015-08-24 03:24:11 +0200

    initial commit

---
+++ dbg.h
@@ -0,0 +1,22 @@
+/*************************************************
+ * author: Adrien Bodineau
+ * github: CodeOverFlow
+ * License: GPL V2
+ ************************************************/
+#ifndef DBG_H
+#define DBG_H
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#ifdef NDEBUG
+#define debug(M,...)
+#define var_val(V,F)
+#define printbits(V,S)
+#else
+#define debug(M,...)fprintf(stderr,"DEBUG(%s:%d,%s):"M"\n",__FILE__,__LINE__,__func__,##__VA_ARGS__)
+#define var_val(V,F)fprintf(stderr,"VALUE(%s:%d,%s):%s="F"\n",__FILE__,__LINE__,__func__,#V,V)
+#define printbits(V,S)for(int i=(S*8)-1;i>=0;i--){putchar('0'+((V>>i)&1));}printf("\t");
+#endif
+#define clean_errno()(errno==0?"NONE":strerror(errno))
+  #define check(P,L,M,...)if(!(P)){fprintf(stderr,"[ERROR](%s:%d,%s-->errno:%s):"M"\n",__FILE__,__LINE__,__func__,clean_errno(),##__VA_ARGS__);errno=0;goto L;}
+#endif
